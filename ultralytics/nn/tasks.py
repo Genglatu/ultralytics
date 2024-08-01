cfab57197aeca8a050b2e55ebd1db4a9ef1cf6f4
Khatami Sahl <40845936+Genglatu@users.noreply.github.com>
2024-08-01 13:43:30 +0700

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -59,6 +59,9 @@ from ultralytics.nn.modules import (
     WorldDetect,
     v10Detect,
     MBConv,
+    conv_bn_hswish,
+    MobileNet_Block,
+    SELayer,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
