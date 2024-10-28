74d1d3926b52edfc1f8ea7e4cba120ac23e15183
Khatami Sahl <40845936+Genglatu@users.noreply.github.com>
2024-10-28 15:39:19 +0700

    Update densenet.py

--- ultralytics/nn/backbone/densenet.py
+++ ultralytics/nn/backbone/densenet.py
@@ -44,103 +44,65 @@ default_cfgs = {
 
 
 class DenseLayer(nn.Module):
-    def __init__(self, num_input_features, growth_rate, bn_size, norm_layer=BatchNormAct2d):
+    def __init__(self, in_channels, growth_rate, bn_size):
         super(DenseLayer, self).__init__()
-        self.add_module('norm1', norm_layer(num_input_features))
-        self.add_module('conv1', nn.Conv2d(num_input_features, bn_size * growth_rate, kernel_size=1, stride=1, bias=False))
-        self.add_module('norm2', norm_layer(bn_size * growth_rate))
-        self.add_module('conv2', nn.Conv2d(bn_size * growth_rate, growth_rate, kernel_size=3, stride=1, padding=1, bias=False))
+        self.layer = nn.Sequential(
+            BatchNormAct2d(in_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels, bn_size * growth_rate, kernel_size=1, stride=1, bias=False),
+            BatchNormAct2d(bn_size * growth_rate),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(bn_size * growth_rate, growth_rate, kernel_size=3, stride=1, padding=1, bias=False)
+        )
 
     def forward(self, x):
-        new_features = self.conv1(self.norm1(x))
-        new_features = self.conv2(self.norm2(new_features))
-        return torch.cat([x, new_features], 1)
-
+        return torch.cat([x, self.layer(x)], 1)
 
 class DenseBlock(nn.Module):
-    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, norm_layer=BatchNormAct2d):
+    def __init__(self, num_layers, in_channels, growth_rate, bn_size):
         super(DenseBlock, self).__init__()
-        self.layer = nn.ModuleList([
-            DenseLayer(num_input_features + i * growth_rate, growth_rate, bn_size, norm_layer=norm_layer)
-            for i in range(num_layers)
-        ])
+        layers = []
+        for i in range(num_layers):
+            layers.append(DenseLayer(in_channels + i * growth_rate, growth_rate, bn_size))
+        self.block = nn.Sequential(*layers)
 
     def forward(self, x):
-        for layer in self.layer:
-            x = layer(x)
-        return x
+        return self.block(x)
 
 class DenseTransition(nn.Sequential):
-    def __init__(self, num_input_features, num_output_features, norm_layer=BatchNormAct2d):
+    def __init__(self, in_channels, out_channels):
         super(DenseTransition, self).__init__()
-        self.add_module('norm', norm_layer(num_input_features))
-        self.add_module('conv', nn.Conv2d(num_input_features, num_output_features, kernel_size=1, stride=1, bias=False))
+        self.add_module('norm', BatchNormAct2d(in_channels))
+        self.add_module('relu', nn.ReLU(inplace=True))
+        self.add_module('conv', nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=False))
         self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
 
-class DenseNet(nn.Module):
-    def __init__(self, layer_name="", idx=0, growth_rate=32, block_config=(6, 12, 24, 16), bn_size=4, num_classes=1000, in_chans=3):
-        super(DenseNet, self).__init__()
-        self.layer_name = layer_name
-        self.idx = idx
-
-        # Initial convolution
-        num_init_features = growth_rate * 2
-        self.features = nn.Sequential(OrderedDict([
-            ('conv0', nn.Conv2d(in_chans, num_init_features, kernel_size=7, stride=2, padding=3, bias=False)),
-            ('norm0', BatchNormAct2d(num_init_features)),
-            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
-        ]))
-
-        # Dense blocks and transition layers
-        num_features = num_init_features
-        self.dense_blocks = nn.ModuleList()
-        self.transition_layers = nn.ModuleList()
-
-        for i, num_layers in enumerate(block_config):
-            block = DenseBlock(num_layers=num_layers, num_input_features=num_features, growth_rate=growth_rate, bn_size=bn_size)
-            self.dense_blocks.append(block)
-            num_features = num_features + num_layers * growth_rate
-            if i != len(block_config) - 1:
-                trans = DenseTransition(num_input_features=num_features, num_output_features=num_features // 2)
-                self.transition_layers.append(trans)
-                num_features = num_features // 2
-
-        # Final batch norm
-        self.features.add_module('norm5', BatchNormAct2d(num_features))
-        self.num_features = num_features
-
-    def forward(self, x):
-        x = self.features(x)
-        for i, block in enumerate(self.dense_blocks):
-            x = block(x)
-            if self.layer_name == "densenet_layer" and self.idx == i:
-                return x
-            if i < len(self.transition_layers):
-                x = self.transition_layers[i](x)
-                if self.layer_name == "densenet_transition_layer" and self.idx == i:
-                    return x
-        return x
-        
 class Densenet121(nn.Module):
-    def __init__(self, cin=3, cout=64, idx=0, layer_name=""):
+    def __init__(self):
         super(Densenet121, self).__init__()
-        self.densenet = DenseNet(layer_name=layer_name, idx=idx, growth_rate=32, block_config=(6, 12, 24, 16), in_chans=cin)
+        self.initial = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
+            BatchNormAct2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+        self.block1 = DenseBlock(num_layers=6, in_channels=64, growth_rate=32, bn_size=4)
+        self.trans1 = DenseTransition(in_channels=256, out_channels=128)
+        self.block2 = DenseBlock(num_layers=12, in_channels=128, growth_rate=32, bn_size=4)
+        self.trans2 = DenseTransition(in_channels=512, out_channels=256)
+        self.block3 = DenseBlock(num_layers=24, in_channels=256, growth_rate=32, bn_size=4)
+        self.trans3 = DenseTransition(in_channels=1024, out_channels=512)
+        self.block4 = DenseBlock(num_layers=16, in_channels=512, growth_rate=32, bn_size=4)
+        self.final_conv = nn.Conv2d(1024, 1024, kernel_size=1)
 
     def forward(self, x):
-        return self.densenet(x)
-
-class Densenet169(nn.Module):
-    def __init__(self, cin=3, cout=64, idx=0, layer_name=""):
-        super(Densenet169, self).__init__()
-        self.densenet = DenseNet(layer_name=layer_name, idx=idx, growth_rate=32, block_config=(6, 12, 32, 32), in_chans=cin)
-
-    def forward(self, x):
-        return self.densenet(x)
-
-class Densenet201(nn.Module):
-    def __init__(self, cin=3, cout=64, idx=0, layer_name=""):
-        super(Densenet201, self).__init__()
-        self.densenet = DenseNet(layer_name=layer_name, idx=idx, growth_rate=32, block_config=(6, 12, 48, 32), in_chans=cin)
-
-    def forward(self, x):
-        return self.densenet(x)
+        x = self.initial(x)
+        x = self.block1(x)
+        x = self.trans1(x)
+        x = self.block2(x)
+        x = self.trans2(x)
+        x = self.block3(x)
+        x = self.trans3(x)
+        x = self.block4(x)
+        x = self.final_conv(x)
+        return x
